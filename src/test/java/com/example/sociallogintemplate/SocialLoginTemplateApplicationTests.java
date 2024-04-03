16df01a97d609f61ae12b7bd9ab63d9829cff9b9
U-keun Song <sukeun319@gmail.com>
2024-04-03 22:06:50 +0900

    first commit

---
+++ src/test/java/com/example/sociallogintemplate/SocialLoginTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sociallogintemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
