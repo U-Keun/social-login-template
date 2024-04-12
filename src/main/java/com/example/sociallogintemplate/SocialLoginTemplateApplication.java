ae6637c1e928c7e8b255755b8109d842cbc108da
U-keun Song <sukeun319@gmail.com>
2024-04-12 13:37:35 +0900

    Chore : application.yml 바인딩을 위한 어노테이션 추가

--- src/main/java/com/example/sociallogintemplate/SocialLoginTemplateApplication.java
+++ src/main/java/com/example/sociallogintemplate/SocialLoginTemplateApplication.java
@@ -1,9 +1,16 @@
 package com.example.sociallogintemplate;
 
+import com.example.sociallogintemplate.config.properties.AppProperties;
+import com.example.sociallogintemplate.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        CorsProperties.class,
+        AppProperties.class
+})
 public class SocialLoginTemplateApplication {
 
     public static void main(String[] args) {
