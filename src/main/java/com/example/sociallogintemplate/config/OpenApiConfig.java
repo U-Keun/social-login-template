905cac6bf4f205287491860bcfc7d2fa8a0b6afe
U-keun Song <sukeun319@gmail.com>
2024-04-12 13:36:07 +0900

    Docs : Swagger 설정 클래스 추가

---
+++ src/main/java/com/example/sociallogintemplate/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.example.sociallogintemplate.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("소셜 로그인 템플릿 프로젝트 API Document")
+                .version("v0.0.1")
+                .description("소셜 로그인 템플릿 프로젝트의 API 명세서입니다.");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
